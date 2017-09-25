913cd399f2c1561ef7705ce8f36da9d582cf75cf
binarycounter <binarycountermusic@gmail.com>
2017-09-25 06:34:40 +0200

    incomplete build script, assembles asm code now

--- build/build.py
+++ build/build.py
@@ -3,8 +3,8 @@ import sys
 print "PkSploit Build Script - Revision 24-09-17"
 
 choice=""
-everything=4;
-dir="dist/";
+everything=4
+dir="dist/"
 while choice not in range(1,5):
 	print "---------------------------"
 	print "What do you want to build?"
@@ -18,15 +18,46 @@ while choice not in range(1,5):
 		choice = int(raw_input('--> '))
 	except:
 		print "Invalid Number"
-		choice = 0;
+		choice = 0
 
-
-if not os.path.exists(dir):
-    os.makedirs(dir)
-	
-if choice == 1 or choice == everything:
+def assemble():
 	print "Assembling Gameboy Code"
 	
 	os.system("rgbasm.exe -o "+dir+"main.o ../gb_asm/main.asm")
 	os.system("rgblink.exe -o "+dir+"main.gb "+dir+"main.o")
+	fi = open(dir+"main.gb","rb")
+	gb_rom = fi.read()
+	fi.close()
+	fo = open(dir+"main.bin", "wb")
+	fo.write(gb_rom[0x150:0x214])
+	fo.close()
+	return
+
+def prepare():
+	print "Preparing Arduino Project"
+
+	if not os.path.exists(dir+"main.bin"):
+		print "Gameboy Code not assembled yet, doing that now..."
+		assemble()
+
+	return
+
+
+def buildexe():
+	print "Building exe not implemented"
+	return
+
+if not os.path.exists(dir):
+	os.makedirs(dir)
+
+
+if choice == 1 or choice == everything:
+	assemble()
+	
+if choice == 2 or choice == everything:
+	prepare()
+	
+if choice == 3 or choice == everything:
+	buildexe()
+
 	
