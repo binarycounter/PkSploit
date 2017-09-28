6db5422037a7cdab985045d5ab3dfcf122673cfe
binarycounter <binarycountermusic@gmail.com>
2017-09-28 07:15:16 +0200

    Arduino now fully implemented, also alot faster.

--- arduino/pksploit_link_bridge/pksploit_link_bridge.ino
+++ arduino/pksploit_link_bridge/pksploit_link_bridge.ino
@@ -18,6 +18,26 @@
 #include "pokemon.h"
 #include "data.h"
 
+//Faster digital read write code from
+//http://masteringarduino.blogspot.de/2013/10/fastest-and-smallest-digitalread-and.html
+#define portOfPin(P)\
+  (((P)>=0&&(P)<8)?&PORTD:(((P)>7&&(P)<14)?&PORTB:&PORTC))
+#define ddrOfPin(P)\
+  (((P)>=0&&(P)<8)?&DDRD:(((P)>7&&(P)<14)?&DDRB:&DDRC))
+#define pinOfPin(P)\
+  (((P)>=0&&(P)<8)?&PIND:(((P)>7&&(P)<14)?&PINB:&PINC))
+#define pinIndex(P)((uint8_t)(P>13?P-14:P&7))
+#define pinMask(P)((uint8_t)(1<<pinIndex(P)))
+
+#define pinAsInput(P) *(ddrOfPin(P))&=~pinMask(P)
+#define pinAsInputPullUp(P) *(ddrOfPin(P))&=~pinMask(P);digitalHigh(P)
+#define pinAsOutput(P) *(ddrOfPin(P))|=pinMask(P)
+#define digitalLow(P) *(portOfPin(P))&=~pinMask(P)
+#define digitalHigh(P) *(portOfPin(P))|=pinMask(P)
+#define isHigh(P)((*(pinOfPin(P))& pinMask(P))>0)
+#define isLow(P)((*(pinOfPin(P))& pinMask(P))==0)
+#define digitalState(P)((uint8_t)isHigh(P))
+
 int volatile CLOCK_PIN = 2;
 int volatile SO_PIN = 6;
 int volatile SI_PIN = 3;
@@ -29,9 +49,11 @@ int ledStatus = 13;
 unsigned volatile long lastReceive = 0;
 volatile byte outputBuffer = 0x00;
 volatile int counterRead = 0;
-
+volatile boolean sending = false;
+volatile trade_centre_state_t nextstate = PKSPLOIT_MENU;
 volatile int counter = 0;
-volatile int counter2 = -1;
+volatile byte command = 0x00;
+volatile int counter2 = 00;
 volatile connection_state_t connection_state = NOT_CONNECTED;
 volatile trade_centre_state_t trade_centre_state = INIT;
 
@@ -47,13 +69,13 @@ void setup() {
   pinMode(CLOCK_PIN, INPUT);
   digitalWrite(CLOCK_PIN, HIGH);
   attachInterrupt( 0, clockInterrupt, RISING );
-  Serial.begin(115200);
+  Serial.begin(28800);
  Serial.println("Boot complete");
 }
 
 
 void clockInterrupt(void) {
- Serial.print(".");
+ //timer=millis();
   byte in;
   unsigned long t = 0;
   if(lastReceive > 0) {
@@ -63,13 +85,13 @@ void clockInterrupt(void) {
       in = 0x00;
     }
   }
-  data = digitalRead(SI_PIN);
-  if(data == HIGH){
+  
+  if(digitalState(SI_PIN) == HIGH){
     val |= ( 1 << (7-counterRead) );
     in |= ( 1 << (7-counterRead) );
   }
   if(counterRead == 7) {
-    
+    //Serial.write((byte)val);
     outputBuffer = handleIncomingByte((byte)val);
     
     
@@ -80,15 +102,25 @@ void clockInterrupt(void) {
   
   counterRead++;
   lastReceive = micros();
-  while( ((digitalRead(CLOCK_PIN) | CLOCK_PIN) & CLOCK_PIN)  == 0);
-  digitalWrite(SO_PIN, outputBuffer & 0x80 ? SO_PIN : 0);
+  while( ((digitalState(CLOCK_PIN) | CLOCK_PIN) & CLOCK_PIN)  == 0);
+  if(outputBuffer & 0x80 ? SO_PIN : 0!=0)
+  {
+  digitalHigh(SO_PIN);
+  }else{digitalLow(SO_PIN);}
+
   outputBuffer = outputBuffer << 1;
+  
 }
 byte handleIncomingByte(byte in) {
    
   byte send = 0x00;
+  
   switch(connection_state) {
   case NOT_CONNECTED:
+    if (in==0xCD)
+    {connection_state = TRADE_CENTRE;
+    trade_centre_state = PKSPLOIT_MENU;
+    Serial.print("StatusMQ");}
     if(in == PKMN_MASTER)
       send = PKMN_SLAVE;
     else if(in == PKMN_BLANK)
@@ -96,21 +128,23 @@ byte handleIncomingByte(byte in) {
     else if(in == PKMN_CONNECTED) {
       send = PKMN_CONNECTED;
       connection_state = CONNECTED;
-                        digitalWrite(ledStatus, HIGH);
+      Serial.print("StatusConnected");   
     }
     break;
 
   case CONNECTED:
     if(in == PKMN_CONNECTED)
       send = PKMN_CONNECTED;
-    else if(in == PKMN_TRADE_CENTRE)
+    else if(in == PKMN_TRADE_CENTRE){
       connection_state = TRADE_CENTRE;
+      Serial.print("StatusTradeCenter");}
     else if(in == PKMN_COLOSSEUM)
       connection_state = COLOSSEUM;
     else if(in == PKMN_BREAK_LINK || in == PKMN_MASTER) {
       connection_state = NOT_CONNECTED;
       send = PKMN_BREAK_LINK;
-                        digitalWrite(ledStatus, LOW);
+      Serial.print("StatusDisconnected");
+                        
     } else {
       send = in;
     }
@@ -120,6 +154,7 @@ byte handleIncomingByte(byte in) {
     if(trade_centre_state == INIT && in == 0x00) {
       if(counter++ == 5) {
         trade_centre_state = READY_TO_GO;
+      Serial.print("StatusSending");
       }
       send = in;
     } else if(trade_centre_state == READY_TO_GO && (in & 0xF0) == 0xF0) {
@@ -142,11 +177,49 @@ byte handleIncomingByte(byte in) {
       send = DATA_BLOCK[counter++];
       if(counter == 619) {
         trade_centre_state = PKSPLOIT_MENU;
+        Serial.print("StatusMenu");
       }
     }
      else if(trade_centre_state == PKSPLOIT_MENU) {
-
+       
+      if(Serial.available()<3 && !sending){trade_centre_state=nextstate;return send;}
       
+      send=Serial.read();
+      if(sending==false){ //Not already sending, must be first byte (aka command)
+      switch (send)
+      {
+        case 34: //Internal CMD 0x22, set nextstate to PKSPLOIT_DUMP_BLOCK
+          nextstate=PKSPLOIT_DUMP_BLOCK;
+          send=00;
+          break;
+        case 68: //Internal CMD 0x44, set nextstate to PKSPLOIT_SEND_BLOCK
+          nextstate=PKSPLOIT_SEND_BLOCK;
+          send=00;
+          break;
+        case 17: //Internal CMD 0x11
+          counter2=Serial.read()*256+Serial.read();
+          send=00;
+          break;
+      }
+      
+      sending=true;}
+      
+      if(!Serial.available()){sending=false;}
+      
+     }
+    else if(trade_centre_state == PKSPLOIT_DUMP_BLOCK) {
+      Serial.write(in);
+      
+      if(counter2==0)
+      {
+        trade_centre_state = PKSPLOIT_MENU;
+        nextstate = PKSPLOIT_MENU;
+        Serial.print("StatusMenu");
+      }
+      counter2--;
+      
+
+
     } else {
       send = in;
     }
@@ -160,7 +233,6 @@ byte handleIncomingByte(byte in) {
     send = in;
     break;
   }
-  lsend=send;
   return send;
 }
 
